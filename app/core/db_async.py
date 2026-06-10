0d5d02668775a371fe1879082a031ff95fdd2a60
Ataur <atauraahmad@gmail.com>
2026-06-10 20:07:28 +0100

    add asyncpg

--- app/core/db_async.py
+++ app/core/db_async.py
@@ -3,11 +3,18 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
 DATABASE_URL_ASYNC = settings.DATABASE_URL_ASYNC
-# PostgreSQL async: "postgresql+asyncpg://user:pass@localhost/db"
 
-engine = create_async_engine(DATABASE_URL_ASYNC, echo=True)
+engine = create_async_engine(
+    DATABASE_URL_ASYNC,
+    echo=True,
+    pool_pre_ping=True,
+)
 
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+AsyncSessionLocal = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 
 class Base(DeclarativeBase):
